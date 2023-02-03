926f62d585bfcfbd6f12c59ee2c439eb1dd0c27e
Sourav Kumar <kmrsourav786@gmail.com>
2023-02-03 22:29:46 +0530

    created new files

---
+++ frontend/paridhan/src/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './dashboard.module.css'
+
+const Dashboard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
--- frontend/paridhan/src/pages/login/Login.jsx
+++ frontend/paridhan/src/pages/login/Login.jsx
@@ -5,6 +5,27 @@ const Login = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <div className={style.bottom}>
+        <form id={style.signin_form} 
+        // onSubmit={onSubmit}
+        >
+          <input
+            // onChange={(e) => onChange(e)}
+            placeholder="Enter Email"
+            type="text"
+            name="email"
+            required
+          />
+          <input
+            // onChange={(e) => onChange(e)}
+            placeholder="Enter Password"
+            type="password"
+            name="password"
+            required
+          />
+          <input type="submit" value="SIGNIN" id="signin" />
+        </form>
+      </div>
     </div>
   )
 }

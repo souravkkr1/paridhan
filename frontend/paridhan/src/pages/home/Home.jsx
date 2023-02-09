5724fd0a92859e569f873b9c03b0d65b71beae24
Sourav Kumar <kmrsourav786@gmail.com>
2023-02-10 00:23:48 +0530

    created all  products section of homepage

--- frontend/paridhan/src/pages/home/Home.jsx
+++ frontend/paridhan/src/pages/home/Home.jsx
@@ -4,6 +4,163 @@ import {Link} from 'react-router-dom'
 import Navbar from '../../components/navbar/Navbar'
 
 const home = () => {
+
+  const new_arrival=
+    [
+      {
+        img:"https://img3.junaroad.com/uiproducts/17718906/zoom_0-1620383566.jpg",
+        price:999,
+        title:"Product 7",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/18823368/zoom_0-1670820278.jpg",
+        price:999,
+        title:"Product 8",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/19147818/zoom_0-1673332950.jpg",
+        price:999,
+        title:"Product 9",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+      img:"https://img3.junaroad.com/uiproducts/18823471/zoom_0-1674018312.jpg",
+      price:999,
+      title:"Product 1",
+      discPrice:399,
+      author:"Sprouted"
+  },
+  {
+      img:"https://img3.junaroad.com/uiproducts/18294842/zoom_0-1647088459.jpg",
+      price:999,
+      title:"Product 2",
+      discPrice:399,
+      author:"Sprouted"
+  }
+    ]
+  
+
+  const women=[
+    {
+        img:"https://img3.junaroad.com/uiproducts/18823471/zoom_0-1674018312.jpg",
+        price:999,
+        title:"Product 1",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/18294842/zoom_0-1647088459.jpg",
+        price:999,
+        title:"Product 2",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/17611516/zoom_0-1616215900.jpg",
+        price:999,
+        title:"Product 3",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/18532551/zoom_0-1663862702.jpg",
+        price:999,
+        title:"Product 4",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/18537395/zoom_0-1664045976.jpg",
+        price:999,
+        title:"Product 5",
+        discPrice:399,
+        author:"Sprouted"
+    },
+]
+
+const men=[
+
+{
+        img:"https://img3.junaroad.com/uiproducts/17718906/zoom_0-1620383566.jpg",
+        price:999,
+        title:"Product 7",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/18823368/zoom_0-1670820278.jpg",
+        price:999,
+        title:"Product 8",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+        img:"https://img3.junaroad.com/uiproducts/19147818/zoom_0-1673332950.jpg",
+        price:999,
+        title:"Product 9",
+        discPrice:399,
+        author:"Sprouted"
+    },
+    {
+      img:"https://img3.junaroad.com/uiproducts/18823471/zoom_0-1674018312.jpg",
+      price:999,
+      title:"Product 1",
+      discPrice:399,
+      author:"Sprouted"
+  },
+  {
+      img:"https://img3.junaroad.com/uiproducts/18294842/zoom_0-1647088459.jpg",
+      price:999,
+      title:"Product 2",
+      discPrice:399,
+      author:"Sprouted"
+  }
+    ]
+
+    const kids= [
+      {
+          img:"https://img3.junaroad.com/uiproducts/18823471/zoom_0-1674018312.jpg",
+          price:999,
+          title:"Product 1",
+          discPrice:399,
+          author:"Sprouted"
+      },
+      {
+          img:"https://img3.junaroad.com/uiproducts/18294842/zoom_0-1647088459.jpg",
+          price:999,
+          title:"Product 2",
+          discPrice:399,
+          author:"Sprouted"
+      },
+      {
+          img:"https://img3.junaroad.com/uiproducts/17611516/zoom_0-1616215900.jpg",
+          price:999,
+          title:"Product 3",
+          discPrice:399,
+          author:"Sprouted"
+      },
+      {
+          img:"https://img3.junaroad.com/uiproducts/18532551/zoom_0-1663862702.jpg",
+          price:999,
+          title:"Product 4",
+          discPrice:399,
+          author:"Sprouted"
+      },
+      {
+          img:"https://img3.junaroad.com/uiproducts/18537395/zoom_0-1664045976.jpg",
+          price:999,
+          title:"Product 5",
+          discPrice:399,
+          author:"Sprouted"
+      },
+  ]
+  
+
   return (
     <>
     <Navbar/>
@@ -51,8 +208,18 @@ const home = () => {
       <div className={style.new_arrivals}>
         <div className={style.container}>
           <h2>NEW ARRIVALS</h2>
-          <div className={style.new_prods}>
-
+          <div className={style.prods}>
+          {
+                  new_arrival.map((item)=>{
+                    return(
+                      <div className={style.prod_box}>
+                        <img src={item.img} alt="" />
+                        <h3>{item.title}</h3>
+                        <p>₹{item.price}</p>
+                      </div>
+                    )
+                  })
+                }
           </div>
         </div>
       </div>
@@ -62,8 +229,18 @@ const home = () => {
       <div className={style.women}>
         <div className={style.container}>
           <h2>WOMEN</h2>
-          <div className={style.women_prod}>
-
+          <div className={style.prods}>
+                {
+                  women.map((item)=>{
+                    return(
+                      <div className={style.prod_box}>
+                        <img src={item.img} alt="" />
+                        <h3>{item.title}</h3>
+                        <p>₹{item.price}</p>
+                      </div>
+                    )
+                  })
+                }
           </div>
         </div>
       </div>
@@ -73,8 +250,18 @@ const home = () => {
       <div className={style.men}>
         <div className={style.container}>
           <h2>MEN</h2>
-          <div className={style.men_prods}>
-
+          <div className={style.prods}>
+          {
+                  men.map((item)=>{
+                    return(
+                      <div className={style.prod_box}>
+                        <img src={item.img} alt="" />
+                        <h3>{item.title}</h3>
+                        <p>₹{item.price}</p>
+                      </div>
+                    )
+                  })
+                }
           </div>
         </div>
       </div>
@@ -84,8 +271,18 @@ const home = () => {
       <div className={style.kids}>
         <div className={style.container}>
           <h2>KIDS</h2>
-          <div className={style.kids_prods}>
-
+          <div className={style.prods}>
+          {
+                  kids.map((item)=>{
+                    return(
+                      <div className={style.prod_box}>
+                        <img src={item.img} alt="" />
+                        <h3>{item.title}</h3>
+                        <p>₹{item.price}</p>
+                      </div>
+                    )
+                  })
+                }
           </div>
         </div>
       </div>

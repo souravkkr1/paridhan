5552cc789fc5b836334f8468e6964c38416e7183
Sourav Kumar <kmrsourav786@gmail.com>
2023-02-23 23:25:11 +0530

    added profile page

---
+++ frontend/paridhan/src/pages/profile/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from './profile.module.css'
+import Navbar from '../../components/navbar/Navbar'
+
+const Profile = () => {
+  return (
+    <>
+    <Navbar/>
+    <div className={style.main}>
+        
+    </div>
+    </>
+    
+  )
+}
+
+export default Profile
